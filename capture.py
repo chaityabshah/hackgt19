5197e12f220c65f83007601deebfb8c0154e535b
Nick Petosa <nick.petosa@gmail.com>
2019-10-26 20:09:44 -0400

    Websocket listener

--- capture.py
+++ capture.py
@@ -7,21 +7,21 @@ from collections import defaultdict
 import threading
 import json
 import os
+import requests
 
 class Capture:
 
     def __init__(self):
 
         self.lock = threading.Lock()
+        self.context = None
         try: self.db = json.load(open("log.json", "r"))
-        except: self.db = defaultdict(lambda: defaultdict(list))
-        print(self.db)
+        except: self.db = {}
 
         def on_message(ws, message):
             data = xmltodict.parse(message, dict_constructor=dict)
             data["timestamp"] = int(time.time())
             self.switch(data)
-
 
         def on_error(self, ws, error):
             print(error)
@@ -36,16 +36,77 @@ class Capture:
         self.ws.run_forever()
 
     def switch(self, message):
-        self.lock.acquire()
-        ts = message["timestamp"]
-        if "updates" in message:
-            deviceID = message["updates"]["@deviceID"]
-            if "volumeUpdated" in message["updates"]:
-                self.db[deviceID]["volume"].append((int(message["updates"]["volumeUpdated"]["volume"]["targetvolume"]), ts))
-                self.db[deviceID]["muteenabled"].append((bool(message["updates"]["volumeUpdated"]["volume"]["muteenabled"]), ts))
-        with open("log.json", "w") as f:
-            json.dump(self.db, f)
+
+
+        def connect(name):
+            if self.context != name:
+                self.context = None
+                print("{} connected.".format(name))
+            self.context = name
+            
+        def try_create(d, key, val):
+            if key not in d: d[key] = val
+
+        try:
+            self.lock.acquire()
+            ts = message["timestamp"]
+            if "updates" in message:
+
+                # Volume
+                if "volumeUpdated" in message["updates"] and self.context is not None:
+                    try_create(self.db[self.context], "volume", [])
+                    packet = {"timestamp":ts}
+                    packet["volume"] = int(message["updates"]["volumeUpdated"]["volume"]["targetvolume"])
+                    packet["muteenabled"] = message["updates"]["volumeUpdated"]["volume"]["muteenabled"] != "false"
+                    print(packet)
+                    self.db[self.context]["volume"].append(packet)
+
+                # Bass update
+                if "bassUpdated" in message["updates"] and self.context is not None:
+                    response = requests.request("GET", "http://192.168.1.174:8090/bass").text
+                    bass = int(xmltodict.parse(response, dict_constructor=dict)["bass"]["targetbass"])
+                    try_create(self.db[self.context], "bass", [])
+                    packet = {"timestamp":ts}
+                    packet["bass"] = bass
+                    print(packet)
+                    self.db[self.context]["bass"].append(packet)
+
+
+                # nowPlayingUpdated
+                if "nowPlayingUpdated" in message["updates"]:
+                    npu = message["updates"]["nowPlayingUpdated"]["nowPlaying"]
+
+                    packet = {"timestamp":ts}
+                    if (npu["@source"] == "SPOTIFY"
+                        and all(x in npu for x in ["track", "artist", "album", "playStatus", "trackID"]) 
+                        and npu["track"] is not None
+                        and npu["playStatus"] == "PLAY_STATE"):
+                        
+                        username = npu["@sourceAccount"]
+                        if username != "":
+                            connect(username)
+                            self.context = username
+                            try_create(self.db, self.context, {})
+                            packet["track"] = npu["track"]
+                            packet["artist"] = npu["artist"]
+                            packet["album"] = npu["album"]
+                            packet["playStatus"] = npu["playStatus"]
+                            packet["image"] = npu["art"]["#text"]
+                            packet["trackID"] = npu["trackID"]
+                            try_create(self.db[self.context], "nowPlaying", [])
+                            if len(self.db[self.context]["nowPlaying"]) == 0 or self.db[self.context]["nowPlaying"][-1]["trackID"] != packet["trackID"]:
+                                print(packet)
+                                self.db[self.context]["nowPlaying"].append(packet)
+
+
+            json.dump(self.db, open('log.json', 'w'))
+
+        except Exception as e:
+            print(e, "died")
+            print(message)
+        
         self.lock.release()
+        #print("released")
 
 
 if __name__ == "__main__":
